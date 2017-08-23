d7732aaa448a5c81f1487727e203adfed7b15fce
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-08-23 16:14:25 +0100

    Random pricing generation.

--- src/main/java/pricing/TicketPrice.java
+++ src/main/java/pricing/TicketPrice.java
@@ -1,5 +1,16 @@
 package pricing;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+
 public class TicketPrice {
+
+    void price() {
+        Random rand = new Random();
+        for (int j = 0; j < 10; j++) {
+            DecimalFormat df = new DecimalFormat("###.00"); // Set your desired format here.
+            System.out.println("$" + df.format(Math.abs(rand.nextInt()) / 10000000.0));
+        }
+    }
 
 }
