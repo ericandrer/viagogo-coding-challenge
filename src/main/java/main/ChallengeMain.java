5b361809b788df51655f18f877002e547279257e
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-08-23 16:15:07 +0100

    Main file plus greetings for required location.

--- src/main/java/main/ChallengeMain.java
+++ src/main/java/main/ChallengeMain.java
@@ -1,5 +1,19 @@
 package main;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ChallengeMain {
 
+    public static void main(String args[]) {
+        System.out.println("Hello! Welcome to Viagogo Coding Challenge.");
+        System.out.println("Please enter your coordinates: ");
+
+        Scanner in = new Scanner(System.in);
+        String coordinates = in.nextLine();
+        String[] vars= coordinates.split(",");
+
+        System.out.println(Arrays.toString(vars));
+
+    }
 }
