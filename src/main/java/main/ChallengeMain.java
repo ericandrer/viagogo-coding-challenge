ee4f14a0d15dcc48871f1d981fc97e4d2d1e2f9c
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-08-23 17:57:59 +0100

    Accepting valid coordinates.

--- src/main/java/main/ChallengeMain.java
+++ src/main/java/main/ChallengeMain.java
@@ -1,19 +1,46 @@
 package main;
 
+import pricing.TicketPrice;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class ChallengeMain {
 
+/*    public static ArrayList<TicketPrice> myArray() {
+        ArrayList<TicketPrice> al = new ArrayList();
+        al.add(new TicketPrice());
+        return al;
+    }*/
+
     public static void main(String args[]) {
         System.out.println("Hello! Welcome to Viagogo Coding Challenge.");
-        System.out.println("Please enter your coordinates: ");
+        boolean myFlag = false;
+        while (!myFlag) {
+            try {
+                System.out.println("Please enter your coordinates: ");
+                Scanner in = new Scanner(System.in);
+                String coordinates = in.nextLine();
+                String[] vars= coordinates.split(",");
+                int x = Integer.parseInt(vars[0]);
+                int y = Integer.parseInt(vars[1]);
+                if (x > 10 || y > 10 || x < -10 || y < -10) {
+                    myFlag = false;
+                    System.out.println("The coordinate ranges are from -10 to +10 (X axis), and -10 to +10 (Y axis).");
+                    System.out.println("E.g: 4,2");
+                } else {
+                    myFlag = true;
+                    System.out.println("Coordinates accepted: " + Arrays.toString(vars));
+                }
+            } catch(NumberFormatException e) {
+                System.out.println("The coordinates are not a valid number or in the correct format.");
+                System.out.println("E.g: 4,2");
+                //e.printStackTrace();
+                myFlag = false;
+            }
 
-        Scanner in = new Scanner(System.in);
-        String coordinates = in.nextLine();
-        String[] vars= coordinates.split(",");
-
-        System.out.println(Arrays.toString(vars));
-
+                //System.out.println(Arrays.toString(vars) + Arrays.toString(new ArrayList[]{myArray()}));
+        }
     }
 }
--- src/main/java/pricing/TicketPrice.java
+++ src/main/java/pricing/TicketPrice.java
@@ -7,10 +7,9 @@ public class TicketPrice {
 
     void price() {
         Random rand = new Random();
-        for (int j = 0; j < 10; j++) {
-            DecimalFormat df = new DecimalFormat("###.00"); // Set your desired format here.
-            System.out.println("$" + df.format(Math.abs(rand.nextInt()) / 10000000.0));
-        }
+        DecimalFormat df = new DecimalFormat("###.00");
+        System.out.println("$" + df.format(Math.abs(rand.nextInt()) / 10000000.0));
+
     }
 
 }
